0c7de400c60bb37323d2aae0301a2a86ac78fd3e
Dennis Herdin <dennis.herdin@web.de>
2014-01-31 22:03:51 +0100

    yet another commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>...</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<!-- Hier kann PHP geübt werden -->
+	<?php  
+	  $a = array(11, 42, 51);
+	  echo $a[1];
+	  $b = array('auto' => ford, 2 => audi);
+	  echo "<br>" . $b[auto];
+	  echo "<br>" . $b[2];
+	  $a[6] = "Mercedes";
+	  echo "<br>" . $a[6];
+	  echo "<br>" . $a[5];
+	?>
+	
+	<p>************************************************</p>
+	<p><strong>Interessante Quellen zu PHP:</strong></p>
+	<ul>
+		<li><a href="http://net.tutsplus.com/tutorials/php/learn-php-from-scratch-a-training-regimen/" target="_blank">http://net.tutsplus.com/tutorials/php/learn-php-from-scratch-a-training-regimen/</a>
+		<li><a href="http://us2.php.net/tut.php" target="_blank">http://us2.php.net/tut.php</a>
+		<li><a href="http://www.w3schools.com/PHP/Default.asp" target="_blank">http://www.w3schools.com/PHP/Default.asp/</a>
+	</ul>
+	<script>
+
+	</script>
+</body>
+
+</html>
